24db00f1599a9fc811cc6cf5655df377c4ac68ec
arafat69 <arafataliar69@gmail.com>
2022-09-08 17:51:40 +0600

    delete thumbnail

--- app/Http/Controllers/API/Rent/RentController.php
+++ app/Http/Controllers/API/Rent/RentController.php
@@ -12,19 +12,19 @@ use Illuminate\Support\Facades\Storage;
 
 class RentController extends Controller
 {
-   
+
     public function index()
     {
         $rents = (new RentRepository())->getAll();
-        return $this->json('Rent list',[
-            'rents'=> RentResource::collection($rents)
+        return $this->json('Rent list', [
+            'rents' => RentResource::collection($rents)
         ]);
     }
 
     public function show($id)
     {
         $rent = (new RentRepository())->findById($id);
-        return $this->json('rent details',[
+        return $this->json('rent details', [
             'rent' => (new RentResource($rent))
         ]);
     }
@@ -32,49 +32,51 @@ class RentController extends Controller
     public function store(RentRequest $request)
     {
         $rent = (new RentRepository())->storeByRequest($request);
-        return $this->json('Post Added successfully',[
+        return $this->json('Post Added successfully', [
             'posts' => (new RentResource($rent))
         ]);
     }
 
     public function update(RentRequest $request, Rent $rent)
     {
-        $rent = (new RentRepository())->updateByRequest($request,$rent);
-        return $this->json('Post updated successfully',[
+        $rent = (new RentRepository())->updateByRequest($request, $rent);
+        return $this->json('Post updated successfully', [
             'posts' => (new RentResource($rent))
         ]);
     }
 
     public function myPosts()
     {
-       $customer = auth()->user()->customer;
-       $posts = $customer->rents()->isActive()->latest()->get();
-       return $this->json('My posts',[
-           'posts' => RentResource::collection($posts)
-       ]);
+        $customer = auth()->user()->customer;
+        $posts = $customer->rents()->isActive()->latest()->get();
+        return $this->json('My posts', [
+            'posts' => RentResource::collection($posts)
+        ]);
     }
 
     public function rentType()
     {
         $type = config('enums.rent_types');
-        return $this->json('Rent Type',[
+        return $this->json('Rent Type', [
             'type' => $type
         ]);
     }
 
     public function destroy(Rent $rent)
     {
-        if($rent){
+        if ($rent) {
             $thumbnails = $rent->thumbnails;
-            if(Storage::exists($thumbnails->src)){
-                Storage::delete($thumbnails->src);
+
+            foreach ($thumbnails as $thumbnail) {
+                if (Storage::exists($thumbnail['path'])) {
+                    Storage::delete($thumbnail['path']);
+                }
             }
+
             $rent->delete();
-            $thumbnails->delete();
 
             return $this->json('Post is deleted successfully');
         }
-        return $this->json('Sorry, Something was wrong',[], Response::HTTP_BAD_REQUEST);
+        return $this->json('Sorry, Something was wrong', [], Response::HTTP_BAD_REQUEST);
     }
-
 }
