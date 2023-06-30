b24873f891afcf2f38a0c527edda62f4f72c95a4
AlzubairMohammed <dev.alzubair@gmail.com>
2023-06-30 18:16:38 +0300

    Edit customer seeder

--- server/database/seeders/CustomerSeeder.php
+++ server/database/seeders/CustomerSeeder.php
@@ -15,20 +15,6 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::factory()->create();
-
-        $customers = [];
-        for($i = 0; $i < 15; $i++){
-            $user = User::factory()->create([
-                'email' => 'customer-'.$i.'@example.com'
-            ]);
-            $customers[] = [
-                'user_id' => $user->id,
-                'created_at' => now()
-            ];
-            $user->assignRole('customer');
-        }
-
-        Customer::insert($customers);
+       
     }
 }
